1a661390fe336286bf00dd0b37791fd969b09ea8
BaharOssareh <78626041+BaharOssareh@users.noreply.github.com>
2024-04-07 18:52:27 -0400

    app.js updated with images and onpress funcs

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
-  let x;
-  x.toString();
+  const handlePress = () => console.log("Text clicked!")
+  console.log(require('./assets/favicon.png')) //require function returns a number not an image
   return (
-    <View style={styles.container}>
-      <Text>Yay! This is my App</Text>
-      <Text>Oh my god! I love iiiiit!😀😀</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Yay! This is my App. Oh my god! I love iiiiit!😀😀 </Text>
+      {/* for local images, this doesn't need size specification */}
+      <Image source={require('./assets/favicon.png')} />
+      {/* for online images, this one needs two more properties in order to be shown */}
+      <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
+        <Image
+          blurRadius={2}
+          // only on android
+          fadeDuration={1000}
+          source={{
+            width: 150,
+            height: 200,
+            uri: "https://picsum.photos/150/200"
+          }} />
+      </TouchableWithoutFeedback>
+      {/* loadingIndicatorSource
+        Similarly to source, this property represents the resource used to render the loading indicator for the image, displayed until image is ready to be displayed, typically after when it got downloaded from network. */}
+      <Image
+        resizeMode='cover'
+        source={{
+          width: 300,
+          height: 200,
+          uri: "https://picsum.photos/300/200"
+        }} />
+      <Image
+        resizeMode='stretch'
+        source={{
+          width: 290,
+          height: 200,
+          uri: "https://picsum.photos/290/200"
+        }} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
